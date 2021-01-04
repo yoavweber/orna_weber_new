4c23b4c726c52754dce71bcefa9c6be5adfe407e
yoavweber <yoav.weber@code.berlin>
2021-01-04 17:11:59 +0100

    gatsbyImg: create getFluid function

---
+++ src/utils/getFluid.js
@@ -0,0 +1,42 @@
+import React, { useMemo } from "react";
+import safeGet from "lodash.get";
+import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
+import BackgroundImage from "gatsby-background-image";
+
+// based on https://github.com/gatsbyjs/gatsby/issues/10482
+const Image = (name) => {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(filter: { internal: { mediaType: { regex: "/image/" } } }) {
+        nodes {
+          relativePath
+          childImageSharp {
+            fluid(
+              maxWidth: 1600
+              quality: 90
+              srcSetBreakpoints: [600, 800, 1280, 1600]
+            ) {
+              ...GatsbyImageSharpFluid_noBase64
+              originalName
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const match = useMemo(
+    () =>
+      data.allFile.nodes.find(
+        (node) => name === node.childImageSharp.fluid.originalName
+      ),
+    [data, name]
+  );
+
+  const fluid = safeGet(match, "childImageSharp.fluid");
+
+  return fluid;
+};
+
+export default Image;
