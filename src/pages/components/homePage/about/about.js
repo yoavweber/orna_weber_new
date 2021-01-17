44a6494243c7164d98bce7db14cb133f8b2c82e9
yoavweber <yoav.weber@code.berlin>
2021-01-17 23:04:35 +0100

    handing images WIP

--- src/pages/components/homePage/about/about.js
+++ src/pages/components/homePage/about/about.js
@@ -2,6 +2,7 @@ import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import ReactMarkdown from "react-markdown";
 import BackgroundImage from "gatsby-background-image";
+import Img from "gatsby-image";
 
 import { H1, Button } from "../../../../layout/common";
 import getFluid from "../../../../utils/getFluid";
@@ -17,17 +18,17 @@ const About = () => {
     button,
     ornaPicture,
   } = data.allStrapiHomePage.edges[0].node.about;
-  const fluid = getFluid("backgroundSmall.png");
-
+  const background = getFluid("backgroundSmall.png");
+  const fluid = ornaPicture.childImageSharp.fluid;
+  console.log(ornaPicture);
   return (
     <section className={styles.section}>
       <div className={styles.wrapper}>
-        <H1 className={styles.headline}>{title} </H1>
+        <div className={styles.imgWrapper}>
+          <H1 className={styles.headline}>{title} </H1>
+          <Img fluid={fluid} className={styles.img} />
+        </div>
         <div className={styles.content}>
-          <img
-            src={`${process.env.API_URL}${ornaPicture.url}`}
-            className={styles.img}
-          />
           <div className={styles.text}>
             <ReactMarkdown allowDangerousHtml={true}>{text}</ReactMarkdown>
             <div className={styles.button}>
@@ -40,7 +41,7 @@ const About = () => {
       </div>
       <div className={styles.backgroundWrapper}>
         <BackgroundImage
-          fluid={fluid}
+          fluid={background}
           Tag="div"
           className={styles.background}
           //   style={{ position: "absolute" }}
@@ -61,7 +62,11 @@ const query = graphql`
             title
             button
             ornaPicture {
-              url
+              childImageSharp {
+                fluid(maxWidth: 200) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
             }
           }
         }
