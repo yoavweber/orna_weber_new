3a0ddcf075a64b4a7a064326d3f425768e774ee3
yoavweber <yoav.weber@code.berlin>
2021-03-15 23:21:54 +0100

    home-page: making intro section responsive

--- src/pages/components/homePage/introText/introText.js
+++ src/pages/components/homePage/introText/introText.js
@@ -1,61 +1,27 @@
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
+
 import showdown from "showdown";
 import ReactMarkdown from "react-markdown";
 
-import { H1, ColoredH1 } from "../../../../layout/common";
+import { ColoredHeadline } from "../../../../layout/common";
 
 import styles from "./introText.module.scss";
 
-// var showdown  = require('showdown'),
-
-const Intro = () => {
-  const data = useStaticQuery(query);
-  const {
-    introText,
-    // introBackground,
-  } = data.allStrapiHomePage.edges[0].node.introText;
-  // const backgroundUrl = introBackground[0].url;
-  // delete it after you choose which md converter to choose
-  // const converter = new showdown.Converter();
-  // converter.setOption("noHeaderId", true);
-  // const htmlData = converter.makeHtml(introText);
+const Intro = ({ data }) => {
+  const { introText, title, introBackground } = data;
+  const fluid = introBackground.localFile.childImageSharp.fluid;
   return (
     <section className={styles.section}>
+      <Img fluid={fluid} className={styles.img} />
       <div className={styles.textWrapper}>
         <div className={styles.text}>
-          {/* <ColoredH1
-            firstPart="את מחוגי השעון אי אפשר להחזיר לאחור,"
-            secondPart="אבל את המראה שלך אפשר להצעיר!"
-          /> */}
+          <ColoredHeadline className={styles.coloredHeadline} text={title} />
           <ReactMarkdown allowDangerousHtml={true}>{introText}</ReactMarkdown>
         </div>
       </div>
-      {/* <img
-        // src={`${process.env.API_URL}${backgroundUrl}`}
-        src={`https://stark-forest-78751.herokuapp.com${backgroundUrl}`}
-        className={styles.img}
-      /> */}
     </section>
   );
 };
-
-const query = graphql`
-  query {
-    allStrapiHomePage {
-      edges {
-        node {
-          id
-          introText {
-            introText
-            # introBackground {
-            #   url
-            # }
-          }
-        }
-      }
-    }
-  }
-`;
 
 export default Intro;
