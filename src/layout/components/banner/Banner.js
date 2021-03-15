df6fd6df73b9fa41aa208e0035281aa16ec4169d
yoavweber <yoav.weber@code.berlin>
2021-03-15 23:24:34 +0100

    banner: adding dynamic color to the subTitle

--- src/layout/components/banner/Banner.js
+++ src/layout/components/banner/Banner.js
@@ -1,28 +1,36 @@
 import React from "react";
 import BackgroundImage from "gatsby-background-image";
+import Img from "gatsby-image";
+import ReactMarkdown from "react-markdown";
 
 import { Button, H1, H2 } from "../../common";
+import getFluid from "../../../utils/getFluid";
+import { splitText } from "../../../utils/utils";
 
 import styles from "./banner.module.scss";
 
 const Banner = ({ data, textStyle }) => {
-  const { img, title, subTitle, button, icon } = data;
-  const fluid = img.localFile.childImageSharp.fluid;
+  const { desktopBackground, headline, subTitle, button, icon } = data;
+  const fluid = desktopBackground.localFile.childImageSharp.fluid;
+  const headlineColor = splitText(subTitle);
+  const isColor = headlineColor.length > 1 ? headlineColor[1] : false;
+  console.log(isColor);
   return (
     <BackgroundImage
       fluid={fluid}
-      Tag="section"
+      Tag="div"
       style={{ width: "100%", height: "100%" }}
       className={styles.wrapper}
       alt="banner picture"
     >
       <div className={styles.contentWrapper}>
         <div className={styles.content}>
-          {/* chnage the H2 to H3 */}
-          <H2 bold>{title}</H2>
-          {subTitle && <H2 color="white"> {subTitle} </H2>}
+          <H2 bold>{headline}</H2>
+          {subTitle && (
+            <H2 color={isColor ? isColor : "white"}> {headlineColor[0]} </H2>
+          )}
           <Button className={styles.button}>{button}</Button>
-          {icon && <img src={icon} width="80" height="80" />}
+          {icon && <img src={icon.url} width="80" height="80" />}
         </div>
       </div>
     </BackgroundImage>
