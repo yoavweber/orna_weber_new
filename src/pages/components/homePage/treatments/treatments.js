fb428b7963797c936a255685cf78b60d1a460dc1
yoavweber <yoav.weber@code.berlin>
2021-06-18 18:51:50 +0200

    homepage: removing slider from treatments

--- src/pages/components/homePage/treatments/treatments.js
+++ src/pages/components/homePage/treatments/treatments.js
@@ -4,22 +4,35 @@ import Img from "gatsby-image";
 import getFluid from "../../../../utils/getFluid";
 
 import { H1 } from "../../../../layout/common";
-import { customMobile } from "../.././../../utils/isMobile";
 import sink from "../../../../layout/assets/icons/sink.svg";
 import needle from "../../../../layout/assets/icons/needle.svg";
-
-import Slider from "../slider/slider";
 
 import styles from "./treatments.module.scss";
 
 //https://erikmartinjordan.com/parent-child-height-width-react-hooks
 const Treatments = () => {
   const [sliderWrapperWidth, setSliderWrapperWidth] = useState();
+  // TODO: use the strapi api and not gatsby
   const imageNames = [
-    { key: 1, href: "#", imgName: "acne.png", name: "אקנה" },
-    { key: 2, href: "#", imgName: "antiAge.png", name: "אנטי-אייג'ינג" },
-    { key: 3, href: "#", imgName: "hairRemoval.png", name: "הסרת שיער" },
-    { key: 4, href: "#", imgName: "scars.png", name: "טשטוש צלקות" },
+    { key: 1, href: "/treatments/acne/", imgName: "acne.png", name: "אקנה" },
+    {
+      key: 2,
+      href: "/treatments/antiAge/",
+      imgName: "antiAge.png",
+      name: "אנטי-אייג'ינג",
+    },
+    {
+      key: 3,
+      href: "/treatments/hairRemoval/",
+      imgName: "hairRemoval.png",
+      name: "הסרת שיער",
+    },
+    {
+      key: 4,
+      href: "/treatments/scars/",
+      imgName: "scars.png",
+      name: "טשטוש צלקות",
+    },
     {
       key: 5,
       href: "#",
@@ -43,17 +56,20 @@ const Treatments = () => {
     return (
       <a key={node.key} href={node.href} className={styles.bubblesWrapper}>
         <Img fluid={fluid} Tag="div" className={styles.bubble} />
-        <span>{node.name}</span>
+        <span style={{ fontSize: "1rem" }}>{node.name}</span>
       </a>
     );
   });
-
+  console.log(sliderWrapperWidth, "!!!");
   return (
     <section className={styles.wrapper}>
-      <img src={sink} alt="treatment icon" className={styles.icon} />
+      <div>
+        <img src={sink} alt="treatment icon" className={styles.icon} />
+      </div>
       <div className={styles.contentWrapper} ref={sliderWrapperRef}>
         <H1>{headline}</H1>
-        {customMobile(1000) ? (
+        <div className={styles.treatmentWrapper}>{treatmentBubble}</div>
+        {/* {customMobile(1000) ? (
           treatmentBubble
         ) : (
           <Slider
@@ -62,9 +78,11 @@ const Treatments = () => {
             showElements={4}
             autoPlay={2000}
           />
-        )}
+        )} */}
       </div>
-      <img src={needle} alt="treatment icon" className={styles.icon} />
+      <div>
+        <img src={needle} alt="treatment icon" className={styles.icon} />
+      </div>
     </section>
   );
 };
