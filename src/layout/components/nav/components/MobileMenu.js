89d1f549f762983fde3627c085e656dad5463c7c
yoavweber <yoav.weber@code.berlin>
2021-06-21 16:24:23 +0200

    nav: handling outside click and removing nav when pressing on link

--- src/layout/components/nav/components/MobileMenu.js
+++ src/layout/components/nav/components/MobileMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "gatsby";
 
 import PhoneIcon from "../../icon/Icon";
@@ -12,9 +12,36 @@ import styles from "../nav.module.scss";
 
 // based on: https://css-tricks.com/hamburger-menu-with-a-side-of-react-hooks-and-styled-components/
 
+function useClickOutside(elRef, callback) {
+  // https://stackoverflow.com/questions/32553158/detect-click-outside-react-component
+  // https://www.youtube.com/watch?v=J-g9ZJha8FE&t=481s&ab_channel=JSConf
+  const callbackRef = useRef();
+  callbackRef.current = callback;
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      console.log(e, "thie is event");
+      if (!elRef?.current?.contains(e.target) && callbackRef.current) {
+        console.log("contain?");
+        callbackRef.current(e);
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [callbackRef, elRef]);
+}
+
 const Menu = ({ links }) => {
-  const [showSubMenu, setShowSubMen] = useState(false);
+  const [showTreatmentMenu, setShowTreatmentMenu] = useState(false);
   const [open, setOpen] = useState(false);
+
+  const toggle = () => setOpen(!open);
+  const hide = () => setOpen(false);
+
+  const wrapperMobileNavRef = useRef();
+  const mobileNavRef = useClickOutside(wrapperMobileNavRef, hide);
 
   const LinkMenu = links.map((node) => {
     if (typeof node.link != "string") {
@@ -22,17 +49,17 @@ const Menu = ({ links }) => {
         <a
           delay="400"
           style={{ cursor: "pointer" }}
-          onClick={() => setShowSubMen(!showSubMenu)}
+          onClick={() => setShowTreatmentMenu(!showTreatmentMenu)}
         >
           <span className={styles.styledSpan}> טיפולים</span>
-          <div className={styles.mobileDropDown} data-click={showSubMenu}>
+          <div className={styles.mobileDropDown} data-click={showTreatmentMenu}>
             {TreatmentMenu}
           </div>
         </a>
       );
     }
     return (
-      <Link to={node.link}>
+      <Link to={node.link} onClick={toggle} onBlur={hide}>
         <span className={styles.styledSpan}>{node.name}</span>
       </Link>
     );
@@ -43,7 +70,11 @@ const Menu = ({ links }) => {
       <nav className={styles.mobileWrapper}>
         <div className={styles.burger}>
           <Burger open={open} setOpen={setOpen} />
-          <div className={styles.mobileMenu} data-showMenu={open}>
+          <div
+            className={styles.mobileMenu}
+            data-showMenu={open}
+            ref={mobileNavRef}
+          >
             {LinkMenu}
           </div>
           <div className={styles.mobileLogo}>
