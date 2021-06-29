fb8d71936a48617109e1340c6b46e3a00f4d966f
yoavweber <yoav.weber@code.berlin>
2021-06-29 12:50:10 +0200

    onnecting form to netlify

--- src/layout/components/form/form.js
+++ src/layout/components/form/form.js
@@ -5,8 +5,8 @@ import styles from "./form.module.scss";
 
 const Form = ({ data }) => {
   const { headline, button } = data;
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
+  // const [name, setName] = useState();
+  // const [email, setEmail] = useState();
   return (
     <section className={styles.section}>
       <div className={styles.wrapper}>
@@ -14,13 +14,21 @@ const Form = ({ data }) => {
           {" "}
           {headline}
         </H1>
-        <form className={styles.form}>
+        {/* TODO: change this to env */}
+        <form
+          className={styles.form}
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+          method="post"
+          name="form"
+        >
           <Input
             className={styles.input}
             type="text"
+            name="name"
             placeholder="שם"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            // value={name}
+            // onChange={(e) => setName(e.target.value)}
             data-aos="zoom-in"
             data-aos-duration="200"
             data-aos-delay="200"
@@ -28,10 +36,11 @@ const Form = ({ data }) => {
           />
           <Input
             className={styles.input}
-            type="email"
-            placeholder="אימייל"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            name="phone"
+            placeholder="טלפון"
+            // value={email}
+            // onChange={(e) => setEmail(e.target.value)}
             data-aos="zoom-in"
             data-aos-duration="200"
             data-aos-delay="400"
@@ -39,8 +48,10 @@ const Form = ({ data }) => {
           />
           <Button
             primary
+            type="submit"
+            form="form"
             style={{ margin: "0" }}
-            onClick={(e) => alert("submited")}
+            // onClick={(e) => alert("submited")}
             className={styles.formButton}
           >
             {button}
